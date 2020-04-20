5756c2727ca71a17004de4c6f682ffb23a34bd5b
lazarust <tlazarus@ksu.edu>
2020-04-20 15:29:07 -0500

    Finishes Searching and Filtering

--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -9,10 +9,64 @@ namespace Movies.Pages
 {
     public class IndexModel : PageModel
     {
+        /// <summary>
+        /// The movies to display on the index page 
+        /// </summary>
+        public IEnumerable<Movie> Movies { get; protected set; }
 
-        public void OnGet()
+        /// <summary>
+        /// Gets the search results for display on the page
+        /// </summary>
+        public void OnGet(double? IMDBMin, double? IMDBMax, int? RottenMin, int? RottenMax)
         {
-
+            
+            this.IMDBMin = IMDBMin;
+            this.IMDBMax = IMDBMax;
+            this.RottenMin = RottenMin;
+            this.RottenMax = RottenMax;
+            MPAARatings = Request.Query["MPAARatings"];
+            Genres = Request.Query["Genres"];
+            Movies = MovieDatabase.Search(SearchTerms);
+            Movies = MovieDatabase.FilterByMPAARating(Movies, MPAARatings);
+            Movies = MovieDatabase.FilterByGenre(Movies, Genres);
+            Movies = MovieDatabase.FilterByIMDBRating(Movies, IMDBMin, IMDBMax);
+            Movies = MovieDatabase.FilterByRottenTomatoes(Movies, RottenMin, RottenMax);
         }
+
+        /// <summary>
+        /// The current search terms 
+        /// </summary>
+        [BindProperty]
+        public string SearchTerms { get; set; } = "";
+
+        /// <summary>
+        /// The filtered MPAA Ratings
+        /// </summary>
+        [BindProperty]
+        public string[] MPAARatings { get; set; }
+
+        /// <summary>
+        /// The filtered genres
+        /// </summary>
+        [BindProperty]
+        public string[] Genres { get; set; }
+
+        /// <summary>
+        /// The minimum IMDB Rating
+        /// </summary>
+        [BindProperty]
+        public double? IMDBMin { get; set; }
+
+        /// <summary>
+        /// The maximum IMDB Rating
+        /// </summary>
+        [BindProperty]
+        public double? IMDBMax { get; set; }
+
+        [BindProperty]
+        public int? RottenMax { get; set; }
+
+        [BindProperty]
+        public int? RottenMin { get; set; }
     }
 }
